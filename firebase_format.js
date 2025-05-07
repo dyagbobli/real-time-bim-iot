469684b8e397d4138a54e6a51358843fed3e2645
dyagbobli <dyagbobli@gmail.com>
2025-05-06 23:36:07 -0500

    Initial commit

---
+++ firebase_format.js
@@ -0,0 +1,34 @@
+{
+  "sensors": {
+    "sensor-1": {
+      "temp": 23.5,
+      "humidity": 55.2,
+      "meta": {
+        "name": "Living Room",
+        "description": "Main sensor in the living room",
+        "groupName": "Level 1",
+        "location": {
+          "x": 31.92,
+          "y": 11.49,
+          "z": -12.97
+        },
+        "objectId": 4124
+      }
+    },
+    "sensor-2": {
+      "temp": 24.8,
+      "humidity": 52.0,
+      "meta": {
+        "name": "Kitchen",
+        "description": "Sensor in the kitchen",
+        "groupName": "Level 1",
+        "location": {
+          "x": 10,
+          "y": 41.64,
+          "z": -12.15
+        },
+        "objectId": 4111
+      }
+    }
+  }
+}
